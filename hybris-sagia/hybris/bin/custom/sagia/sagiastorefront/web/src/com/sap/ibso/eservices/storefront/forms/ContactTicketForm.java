12d490f39a17a027fa56f61680bdaf836b877ea1
Sarath <external.Saisarath.Lalagari@in.bosch.com>
2022-01-22 13:17:31 +0530

    Changes related to contactticketform object

---
+++ hybris-sagia/hybris/bin/custom/sagia/sagiastorefront/web/src/com/sap/ibso/eservices/storefront/forms/ContactTicketForm.java
@@ -0,0 +1,15 @@
+package com.sap.ibso.eservices.storefront.forms;
+
+
+public class ContactTicketForm {
+
+    private String comment;
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+}

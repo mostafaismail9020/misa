d16429491058631ae050a8464a2f694fdf335312
Venkat <external.venkataramana.raogudla@in.bosch.com>
2022-02-04 19:31:24 +0530

    banner and some bugs - raja

--- hybris-sagia/hybris/bin/custom/sagia/sagiastorefront/web/webroot/_ui/responsive/theme-alpha/js/sagia.dashboardsCommon.js
+++ hybris-sagia/hybris/bin/custom/sagia/sagiastorefront/web/webroot/_ui/responsive/theme-alpha/js/sagia.dashboardsCommon.js
@@ -395,6 +395,7 @@ $(document).ready(function() {
 $('#dashboard-carousel').owlCarousel({
     loop: true,
     margin: 20,
+    rtl:true,
      /*
     animateOut: 'fadeOut',
     animateIn: 'fadeIn',

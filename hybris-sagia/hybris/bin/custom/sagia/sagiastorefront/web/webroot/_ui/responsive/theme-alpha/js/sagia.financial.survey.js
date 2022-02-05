844e10466277abbac3a9fac3956e5baaffabde0c
Moahmed Anouar Badri <mohamed.anouar.badri@sap.com>
2022-02-05 19:23:24 +0000

    Financial Survey: treasure field should not accept text

--- hybris-sagia/hybris/bin/custom/sagia/sagiastorefront/web/webroot/_ui/responsive/theme-alpha/js/sagia.financial.survey.js
+++ hybris-sagia/hybris/bin/custom/sagia/sagiastorefront/web/webroot/_ui/responsive/theme-alpha/js/sagia.financial.survey.js
@@ -153,14 +153,18 @@ $(document).ready(function () {
                 return false;
             }
         });
-        /*$("#treasurySharesCurrentQuarterId").keypress(function (e) {
+        $("#treasurySharesCurrentQuarterId").keypress(function (e) {
             //if the letter is not digit then display error and don't type anything
-            if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
+            if (  e.which != 45 && e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
+                //display error message
+                $("#errmsg").html("Digits Only").show().fadeOut("slow");
+                return false;
+            }if ( $("#treasurySharesCurrentQuarterId").val() != '' && e.which == 45 ) {
                 //display error message
                 $("#errmsg").html("Digits Only").show().fadeOut("slow");
                 return false;
             }
-        });*/
+        });
         $("#headOfficeAccountInBranchCurrentQuarterId").keypress(function (e) {
             //if the letter is not digit then display error and don't type anything
             if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {

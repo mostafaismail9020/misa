4a63694aa42ff944e2b23b41e7a5c18c962ae4e0
S0011778297 <b.ramakrishnan@misa.gov.sa>
2022-03-01 14:11:07 +0300

    code committed for installing captcha addon in B2B

--- hybris-sagia/deploy/pre_deploy.sh
+++ hybris-sagia/deploy/pre_deploy.sh
@@ -137,6 +137,7 @@ else
    ant addoninstall -Daddonnames=commerceorgaddon -DaddonStorefront.yacceleratorstorefront=investsaudistorefront
    ant addoninstall -Daddonnames=smarteditaddon -DaddonStorefront.yacceleratorstorefront=investsaudistorefront,sagiastorefront 
    ant addoninstall -Daddonnames=captchaaddon -DaddonStorefront.yacceleratorstorefront=sagiastorefront
+   ant addoninstall -Daddonnames=investsaudicaptchaddon -DaddonStorefront.yacceleratorstorefront=investsaudistorefront
   ./hybrisserver.sh start || handle_error "Hybris failed to start"
 fi
 

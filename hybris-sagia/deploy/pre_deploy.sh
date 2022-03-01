31dc326e188269ea62a69c22554af2a56e328f83
S0011778297 <b.ramakrishnan@misa.gov.sa>
2022-03-01 18:30:12 +0300

    code committed for gulp file

--- hybris-sagia/deploy/pre_deploy.sh
+++ hybris-sagia/deploy/pre_deploy.sh
@@ -138,6 +138,7 @@ else
    ant addoninstall -Daddonnames=smarteditaddon -DaddonStorefront.yacceleratorstorefront=investsaudistorefront,sagiastorefront 
    ant addoninstall -Daddonnames=captchaaddon -DaddonStorefront.yacceleratorstorefront=sagiastorefront
    ant addoninstall -Daddonnames=investsaudicaptchaddon -DaddonStorefront.yacceleratorstorefront=investsaudistorefront
+   chmod 777 /hybris/bin/custom/sagia/sagiastorefront/node_modules/.bin/gulp
    ant sagiastorefront_compileuisrc
   ./hybrisserver.sh start || handle_error "Hybris failed to start"
 fi

c6f030405c2f1a24c951293b7a965a14ec2e8886
Guilherme Nogueira <pericles.ferraz@sap.com>
2022-08-27 12:23:49 -0300

    Adjust invest terms EN page UI
    - css and js
    - data table
    - breadcrums

--- hybris-sagia/hybris/bin/custom/sagia/sagiastorefront/web/webroot/_ui/responsive/theme-alpha/js/sagia.investmentTerms.js
+++ hybris-sagia/hybris/bin/custom/sagia/sagiastorefront/web/webroot/_ui/responsive/theme-alpha/js/sagia.investmentTerms.js
@@ -1,61 +1,48 @@
-investmentTermsVar =  {};
-
 function loadInvestmentTermsData() {
-            $.ajax({
-                url: ACC.config.encodedContextPath + "/economic/investmentsTermsData",
-                method: 'GET',
-                ajaxHideLoadingIndicator: true,
-                success: function (investmentTerms) {
-                    //setupTable(investmentTermsVar);
-                    var $investmentsTermsTable = $('#investmentsTermsId').empty();
-                    if (investmentTerms && investmentTerms.sgiaIndicatorTerms) {
-                        investmentTerms.sgiaIndicatorTerms.forEach(function (investmentTerm) {
-                            var code = investmentTerm.code;
-//                            var name = investmentTerm.code;
-//                            var $investmentTermRow = $('.investmentsTermsTemplate').first().clone(true);
-//                                $investmentTermRow.attr("id", code).children().first().html(code).next().text(name).next().text(name);
-//                                setColorForDraftRow($investmentTermRow);
-//                                investmentTermsVar.dataTable.row.add($investmentTermRow).draw();
-//                            $investmentsTermsTable.append($investmentTermRow);
-                            console.log("investmentTerm", investmentTerm)
-                            $investmentsTermsTable.append('<tr id="'+code+'"><td>'+code+'</td><td>'+investmentTerm.dataPoint+'</td><td>'+investmentTerm.description+'</td><td>'+investmentTerm.calculation+'</td><td>'+investmentTerm.measurement+'</td><td>'+investmentTerm.source+'</td></tr>');
-                        });
-                        $('#investmentsTermsTableId').DataTable();
-                    }
-                },
-                error: function() {
-                    // Display error
-                }
-            });
+    $.ajax({
+        url: ACC.config.encodedContextPath + "/economic/investmentsTermsData",
+        method: 'GET',
+        ajaxHideLoadingIndicator: true,
+        success: function (investmentTerms) {
+            var $investmentsTermsTable = $('#investmentsTermsId').empty();
+            if (investmentTerms && investmentTerms.sgiaIndicatorTerms) {
+                investmentTerms.sgiaIndicatorTerms.forEach(function (investmentTerm) {
+                    var code = investmentTerm.code;
+                    $investmentsTermsTable.append('<tr id="'+code+'"><td>'+code+'</td><td>'+investmentTerm.dataPoint+'</td><td>'+investmentTerm.description+'</td><td>'+investmentTerm.calculation+'</td><td>'+investmentTerm.measurement+'</td><td>'+investmentTerm.source+'</td></tr>');
+                });
+            }
+            setupTable();
+        },
+        error: function() {
+            // Display error
+        }
+    });
 
-    };
+};
 
 var setupTable = function() {
 
     var config = {
         "drawCallback": function () {
-            $('#branchesTableId_length').remove();
-            $('#branchesTableId_filter').addClass('searchInputBox searchInputBox_dark').css('margin-bottom', '15px');
+            $('#investmentsTermsTableId_length').remove();
+            $('#investmentsTermsTableId_filter').addClass('searchInputBox searchInputBox_dark').css('margin-bottom', '15px');
             var searchBranchText = getI18nText("search.branch.placehoder");
-            var $inputSearch = $('#branchesTableId_filter input[type="search"]').addClass('searchInputBox-input').attr('placeholder', searchBranchText);
+            var $inputSearch = $('#investmentsTermsTableId_filter input[type="search"]').addClass('searchInputBox-input').attr('placeholder', searchBranchText);
             var $labelSearch = $inputSearch.closest("label");
             $labelSearch.replaceWith($inputSearch);
 
-            var $pagination = $('#branchesTableId_paginate').addClass('paginationModule-wrapper').wrap('<div class="paginationModule"></div>');
-            $pagination.find('span a').addClass('paginationModule-link').css('cursor', 'pointer').wrap('<div class="paginationModule-item"></div>');
-            $pagination.find('span a.current').addClass('active');
-
-            var $icon = $('#paginationElementId').find('svg').attr('height', '24').attr('width', '24');
-            $pagination.find('a.previous').empty().html($icon.clone()).prop("disabled", true).addClass('paginationModule-control paginationModule-control_left');
-            $pagination.find('a.next').empty().html($icon.clone()).addClass('paginationModule-control paginationModule-control_right');
-        }
+            var $pagination = $('#investmentsTermsTableId_paginate');
+            $pagination.find('a.previous').empty().html('<img src="/_ui/responsive/common/images/arrow-right.png" class="img-responsive transform-180-degree">').addClass('paginationModule-control paginationModule-control_left');
+            $pagination.find('a.next').empty().html('<img src="/_ui/responsive/common/images/Icon-feather-arrow-left.png" class="img-responsive transform-180-degree">').addClass('paginationModule-control paginationModule-control_right');
+        },
+        //"lengthMenu": [ 10, 25, 50, 75, 100 ]
     };
 
     if (SAGIA.locale === 'ar' && SAGIA.dataTablesArabic) {
         config.language = SAGIA.dataTablesArabic;
     }
 
-    investmentTermsVar.dataTable = $('#investmentsTermsTableId').dataTable(config);
+    $('#investmentsTermsTableId').dataTable(config);
 };
 
 

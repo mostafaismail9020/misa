170806cfd984aea5aa2adc1dbe95c7609bd4bba9
AnkP <ankit.patel01@sap.com>
2022-09-01 14:40:17 +0530

    Updated NafathLogin clean up job dao

--- hybris-sagia/hybris/bin/custom/sagia/sagiacore/src/com/sap/ibso/eservices/core/sagia/dao/impl/DefaultNafathDAO.java
+++ hybris-sagia/hybris/bin/custom/sagia/sagiacore/src/com/sap/ibso/eservices/core/sagia/dao/impl/DefaultNafathDAO.java
@@ -5,26 +5,28 @@ import com.sap.ibso.eservices.core.model.SagiaLicenseModel;
 import com.sap.ibso.eservices.core.sagia.dao.NafathDAO;
 import de.hybris.platform.servicelayer.search.FlexibleSearchService;
 import de.hybris.platform.servicelayer.search.SearchResult;
+import de.hybris.platform.servicelayer.time.TimeService;
+import org.joda.time.DateTime;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 public class DefaultNafathDAO implements NafathDAO {
 
     private FlexibleSearchService flexibleSearchService;
+    private TimeService timeService;
 
     @Override
     public List<NafathLoginModel> getOldLoginRecords(Integer daysOld) {
+        Date tillDate = new DateTime(getTimeService().getCurrentTime()).minusDays(daysOld).toDate();
 
-        long tillDate = System.currentTimeMillis() - TimeUnit.DAYS.toMillis(daysOld);
         final StringBuilder query = new StringBuilder();
+        query.append(" SELECT {").append(NafathLoginModel.PK).append("} FROM {").append(NafathLoginModel._TYPECODE)
+                .append("} WHERE {").append(NafathLoginModel.MODIFIEDTIME).append("} <= ?tillDate");
 
-        query.append(" SELECT ").append(NafathLoginModel.PK).append(" FROM ").append(NafathLoginModel._TYPECODE)
-                .append("WHERE ").append(NafathLoginModel.MODIFIEDTIME).append(" < ?tillDate");
-
-        final HashMap<String, String> queryParams = new HashMap<>();
-        queryParams.put("tillDate", String.valueOf(tillDate));
+        final HashMap<String, Date> queryParams = new HashMap<>();
+        queryParams.put("tillDate", tillDate);
 
         final SearchResult<NafathLoginModel> result = getFlexibleSearchService().search(query.toString(), queryParams);
         return result.getResult();
@@ -64,5 +66,13 @@ public class DefaultNafathDAO implements NafathDAO {
 
     public void setFlexibleSearchService(FlexibleSearchService flexibleSearchService) {
         this.flexibleSearchService = flexibleSearchService;
+    }
+
+    public TimeService getTimeService() {
+        return timeService;
+    }
+
+    public void setTimeService(TimeService timeService) {
+        this.timeService = timeService;
     }
 }
